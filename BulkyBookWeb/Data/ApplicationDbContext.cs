e9806f444cff670788f91e29b897c5534e30b3aa
Adir <adir.jscode@gmail.com>
2023-08-29 16:21:02 +0600

    Bootswatch added

--- BulkyBookWeb/Data/ApplicationDbContext.cs
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -10,5 +10,14 @@ namespace BulkyBookWeb.Data
 
         }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Sci fi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Hrror", DisplayOrder = 3}
+                );
+        }
     }
 }

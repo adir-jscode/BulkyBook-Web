679d5b01e37db1dd1c177c2674d9e042270fad0c
Adir <adir.jscode@gmail.com>
2023-08-30 22:53:47 +0600

    server side validation

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -10,8 +10,11 @@ namespace BulkyBookWeb.Models
         [Required]
 
         [DisplayName("Category Name")]
+        [MaxLength(255)]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+
+        [Range(1,100,ErrorMessage ="Order must be between 1 to 100")]
         public int DisplayOrder { get; set; }
     }
 }

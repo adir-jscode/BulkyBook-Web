5ec4c4b87bf966248fda041d53a1fe910e544ac0
Adir <adir.jscode@gmail.com>
2023-08-30 16:19:15 +0600

    Input tag helpers added..[DisplayName], asp-for

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
 {
@@ -7,8 +8,10 @@ namespace BulkyBookWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        public string Name { get; set; }
 
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }

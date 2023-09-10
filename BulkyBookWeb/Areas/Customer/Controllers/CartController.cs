adfe91cf80d72721648839cc945b9049d9e88d6f
Adir <adir.jscode@gmail.com>
2023-09-11 01:23:29 +0600

    Shopping Cart Ui added

---
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Customer.Controllers
+{
+    public class CartController : Controller
+    {
+
+        [Area("Customer")]
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

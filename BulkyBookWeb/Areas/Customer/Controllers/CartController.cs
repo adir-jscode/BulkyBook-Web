bfce44f9728136652df6d153063ffc7aac3e8efd
Adir <adir.jscode@gmail.com>
2023-09-11 09:15:16 +0600

    shoppingcartVM updated

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,16 +1,35 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Bulky.Models;
+using Bulky.Models.ViewModels;
+using BulkyBookWeb.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
 {
     public class CartController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM shoppingCartVM { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
 
         [Area("Customer")]
         [Authorize]
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            shoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u=>u.ApplicationUserId == userId, includeProperties : "Product")
+            };
+
+            return View(shoppingCartVM);
         }
     }
 }
--- BulkyBookWeb/Repository/IRepository/Repository.cs
+++ BulkyBookWeb/Repository/IRepository/Repository.cs
@@ -44,9 +44,13 @@ namespace BulkyBookWeb.Repository.IRepository
             return query.FirstOrDefault();
         }
 
-        public IEnumerable<T> GetAll(string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
             if(!string.IsNullOrEmpty(includeProperties))
             {
                 foreach(var inclideProp in  includeProperties.Split(new char[] {','},StringSplitOptions.RemoveEmptyEntries))
